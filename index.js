b01a8d99480074578d3d5978c6e02109acdc5918
kronbauer67 <kronbauer01@gmail.com>
2024-03-12 11:26:34 -0300

    projeto-heroi

---
+++ index.js
@@ -0,0 +1,24 @@
+let NivelHeroi = 9000
+let Nome = "kron"
+for (let i = 0; i <= 11; i++){
+    NivelHeroi +=1
+    console.log("ganhou xp " + i )
+}
+console.log(NivelHeroi + " totais")
+if (NivelHeroi <1000){
+    console.log("O Herói de nome " + Nome + " está no nível ferro");
+} else if(NivelHeroi <2000){ 
+    console.log("O Herói de nome " + Nome + " está no nível bronze");
+} else if (NivelHeroi < 5000 ) {
+    console.log("O Herói de nome " + Nome + " está no nível prata");
+} else if(NivelHeroi<7000){
+    console.log("O Herói de nome " + Nome + " está no nível ouro");
+} else if (NivelHeroi<8000){
+    console.log("O Herói de nome " + Nome + " está no nível platina");
+}else if (NivelHeroi<9000){
+    console.log("O Herói de nome " + Nome + " está no nível ascendente");
+}else if (NivelHeroi<1000){
+    console.log("O Herói de nome " + Nome + " está no nível imortal");
+}else(NivelHeroi>1001);{
+    console.log("O Herói de nome " + Nome + " está no nível radiante");
+}
